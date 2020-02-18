f6ab47fd96453421e2cbc259db3cd11d61d38262
Philipp Hamm <philipp.hamm@gmail.com>
2020-02-18 15:36:02 +0100

    Improved

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'tasks', to: 'tasks#index', as: :tasks
-  get 'tasks/:id', to: 'tasks#show', as: :task
   get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
   post 'tasks', to: 'tasks#create'
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
